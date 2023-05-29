48e73d69c05c8fcc06029cbc581481b0be455153
Jean-Dan10 <jean.daniel.laplante@gmail.com>
2023-05-29 13:58:50 -0400

    MAJ script creation table compte,Personnage,Classes,Armes.sql

--- Autres_ressources/script creation table compte,Personnage,Classes,Armes.sql
+++ Autres_ressources/script creation table compte,Personnage,Classes,Armes.sql
@@ -8,15 +8,18 @@
                          id_compte int NOT NULL AUTO_INCREMENT,
                          nom_utilisateur varchar(60),
 						 date_creation date,
-                         premiun boolean,
-                         nombre_personnage int,
+                         premium boolean,
+                         nombre_personnages int,
                          
                          CONSTRAINT id_compte_pk PRIMARY KEY (id_compte)
 );
-INSERT INTO Compte (nom_utilisateur,date_creation,premiun,nombre_personnage) VALUES
+INSERT INTO Compte (nom_utilisateur,date_creation,premium,nombre_personnages) VALUES
                               ('Chat','2023-05-23',false,1),
                               ('Orignal','2023-05-19',true,2),
-                              ('Marmotte','1991-09-20',true,10);
+                              ('Marmotte','1991-09-20',true,10),
+                              ('Tyzral','2001-05-03',true,99),
+                              ('Knik','1991-09-20',true,99)
+                              ;
 
  CREATE TABLE Classes (
                          id_classe int NOT NULL AUTO_INCREMENT,
@@ -29,25 +32,31 @@ INSERT INTO Compte (nom_utilisateur,date_creation,premiun,nombre_personnage) VAL
                          CONSTRAINT id_classe_pk PRIMARY KEY (id_classe)
 );
 INSERT INTO Classes (nom_classe,puissance,agileté,constitution,intelligence) VALUES
-                              ('Barbare',5,3,5,1);
+                              ('Barbare',5,3,5,1),
+                              ('Magichat',1,3,2,5),
+                              ('Voleugnal',2,5,3,2),
+                              ('ChaGnal',3,3,3,3)
+                                                            ;
                               
   
  -- création table Armes
- CREATE TABLE Armes (
+ CREATE TABLE Arme (
                          id_arme int NOT NULL AUTO_INCREMENT,
                          nom_arme varchar(60),
-						 dommage int,
-                         portee int,
-                         precission int,
-                         type_stat varchar(60) ,
+						 dommage double,
+                         portee double, -- en metre
+                         precission int, -- sur 100
+                         type_stat varchar(60) , -- str,agi,int
                          CONSTRAINT id_arme_pk PRIMARY KEY (id_arme)
 );
-INSERT INTO Armes (nom_arme,dommage,portee,precission,type_stat) VALUES
-                              ('Hachette',10,2,0.8,'puissance') ;                             
+INSERT INTO Arme (nom_arme,dommage,portee,precission,type_stat) VALUES
+                              ('Hachette',10,1,80,'str') ,
+                               ('baton chatcroyable',3,1.5,100,'int') ,
+                               ('dague',10,0.5,95,'agi');
 
 
 -- création table Personnages
-CREATE TABLE Personnages (
+CREATE TABLE Personnage (
 						 id_personnage int not null AUTO_INCREMENT,
                          nom_personnage varchar(60) not null,
                          id_classe int,
@@ -57,16 +66,12 @@ CREATE TABLE Personnages (
                          id_compte varchar(60) ,
                          CONSTRAINT id_personnage_pk PRIMARY KEY (id_personnage),
                          CONSTRAINT classes_id_classe_FK    FOREIGN KEY ( id_classe) REFERENCES Classes(id_classe),
-                         CONSTRAINT Armes_id_arme_FK    FOREIGN KEY ( id_arme) REFERENCES Armes(id_arme)
+                         CONSTRAINT Arme_id_arme_FK    FOREIGN KEY ( id_arme) REFERENCES Arme(id_arme)
 );
-INSERT INTO Personnages (nom_personnage,id_classe,id_arme,date_creation,niveau,id_compte) VALUES
-                              ('ChatHeroique', 1, 1 , '2023-05-23',41,1);
+INSERT INTO Personnage (nom_personnage,id_classe,id_arme,date_creation,niveau,id_compte) VALUES
+                              ('ChatHeroique', 2, 1 , '2023-05-23',41,1),
+                              ('ChatGratte',3,3,'2023-05-29',1,1);
                              
-                             
-                              
-      
- 
-
-                              
- 
--- validation                               
+-- validation
+select * from Arme;                             
+                         
